e770c7250c5f381781d6b0c595e239d80d20eb36
Scott Gardner <scottgardner23@gmail.com>
2025-05-03 22:45:51 +1000

    wrote during election coverage

--- main.go
+++ main.go
@@ -1,10 +1,45 @@
 package main
 
 import (
+	"math/rand/v2"
 	"fmt"
 	"os"
 	"golang.org/x/term"
 )
+
+func moveCursor(x int, y int) {
+	// ESC [ LINE ; COL H
+	fmt.Printf("\033[%d;%dH", y, x)
+}
+
+func runCommand(com string, temp string, width int, height int) {
+	// fmt.Print(com)
+	switch com {
+	case "save":
+		temp_file_name := fmt.Sprintf("test_file_%d.txt", rand.IntN(100))
+		fo, err := os.Create(temp_file_name)
+		if err != nil {
+			panic(err)
+		}
+		defer func() {
+			if err := fo.Close(); err != nil {
+				panic(err)
+			}
+		}()
+		if _, err := fo.Write([]byte(temp)); err != nil {
+			panic(err)
+		}
+
+
+
+
+		moveCursor(0, height)
+		for _ = range(width) {
+			fmt.Print(" ")
+		}
+		fmt.Printf("Saved file in %s", temp_file_name)
+	}
+}
 
 func main() {
 	oldState, err := term.MakeRaw(int(os.Stdin.Fd()))
@@ -22,8 +57,20 @@ func main() {
 	// CSI 2J == cursor to top left
 	fmt.Print("\033[H\033[2J")
 
-	// Need to use a longer buffer because ESC == ESC but arrow keys are eg ESC [ A
-	buffer := make([]byte, 1)
+	width, height, err := term.GetSize(int(os.Stdout.Fd()))
+	if err != nil {
+		panic(err)
+	}
+	// fmt.Printf("Terminal size: %d x %d\r\n", width, height)
+
+	cursorPosition := []int{0,0}
+	commandCursorPosition := 0
+	IN_COMMAND_MODE := false
+	command := ""
+	fileContents := ""
+	// Using a 3 byte buffer because the arrow keys occur on the third index of
+	// an ESC command (ESC [ A,B,C,D)
+	buffer := make([]byte, 3)
 	MAIN_LOOP:
 	for {
 		_, err := os.Stdin.Read(buffer)
@@ -40,12 +87,87 @@ func main() {
 			break MAIN_LOOP
 		// ESC
 		case 27:
-			break MAIN_LOOP
+			// fmt.Printf("\n\rBuffer: %v", buffer)
+			if buffer[1] == 0 && buffer[2] == 0 {
+				// Just a pure ESC
+				// break MAIN_LOOP
+				// ESC [ LINE ; COL H
+				if IN_COMMAND_MODE {
+					// fmt.Printf("\033[%d;%dH", cursorPosition[0], commandCursorPosition)
+					moveCursor(cursorPosition[0], cursorPosition[1])
+					IN_COMMAND_MODE = false
+				} else {
+					// fmt.Printf("\033[%d;%dH", height, commandCursorPosition)
+					moveCursor(commandCursorPosition, height)
+					IN_COMMAND_MODE = true
+				}
+			} else {
+				// Arrow key
+				if buffer[2] == 65 && cursorPosition[1] > 0 {
+					// UP
+					cursorPosition[1] -= 1
+				} else if buffer[2] == 66 && cursorPosition[1] < height {
+					// DOWN
+					cursorPosition[1] += 1
+				} else if buffer[2] == 67 && cursorPosition[0] < width {
+					// RIGHT
+					cursorPosition[0] += 1
+				} else if buffer[2] == 68 && cursorPosition[0] > 0 {
+					// LEFT
+					cursorPosition[0] -= 1
+				}
+			}
+			// break MAIN_LOOP
+		// BACKSPACE?
+		case 127:
+			// fmt.Print("DEBUG")
+			if IN_COMMAND_MODE {
+				command = command[:len(command)]
+				moveCursor(len(command), height)
+				fmt.Print(" ")
+				moveCursor(len(command), height)
+				commandCursorPosition -= 1
+			} else {
+				// Handle cross line later
+				if cursorPosition[0] > 0 {
+					fileContents = fileContents[:len(fileContents)]
+					cursorPosition[0] -= 1
+					moveCursor(cursorPosition[0], cursorPosition[1])
+					fmt.Print(" ")
+					moveCursor(cursorPosition[0], cursorPosition[1])
+				}
+			}
 		// ENTER
 		case 13:
-			fmt.Print("\r\n")
+			if IN_COMMAND_MODE {
+				runCommand(command, fileContents, width, height)
+				command = ""
+				commandCursorPosition = 0
+				moveCursor(cursorPosition[0], cursorPosition[1])
+				IN_COMMAND_MODE = false
+			} else {
+				// fmt.Print("\r\n")
+				cursorPosition[0] = 0
+				cursorPosition[1] += 1
+				moveCursor(cursorPosition[0], cursorPosition[1])
+				fileContents += "\r\n"
+				// fmt.Printf("Cursor Positions: %d, %d", cursorPosition[0], cursorPosition[1])
+			}
 		default:
+			// fmt.Printf("%v", buffer)
 			fmt.Printf("%c", key)
+			if IN_COMMAND_MODE {
+				commandCursorPosition += 1
+				command += fmt.Sprintf("%c", key)
+			} else {
+				cursorPosition[0] += 1
+				fileContents += fmt.Sprintf("%c", key)
+			}
+		}
+		// Quickly reset the buffer
+		// buffer = []byte{0,0,0,0}
+		for bi := range(len(buffer)) {
+			buffer[bi] = 0
 		}
 	}
 
